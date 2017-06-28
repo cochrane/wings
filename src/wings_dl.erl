8b81db136b4500b49636bfd02e508b9b1932850d
Dan Gudmundsson <dgud@users.sourceforge.net>
2017-06-28 21:21:28 +0200

    debug

--- src/wings_dl.erl
+++ src/wings_dl.erl
@@ -146,8 +146,8 @@ fold(Fun, Acc) ->
 %%  the term.
 
 call(none, State) -> State;
-call({call,Dl,_}, State) ->
-    %%    erlang:display({call, element(2, _Vbo)}),
+call({call,Dl,_Vbo}, State) ->
+    erlang:display({call, element(2, _Vbo)}),
     call(Dl, State);
 call({call_in_this_win,Win,Dl}, State) ->
     case wings_wm:this() of
@@ -329,5 +329,5 @@ update_seen_1(_, Seen) -> Seen.
 
 
 delete_buffers(NotUsed, _From) ->
-    %% erlang:display({delete,{?MODULE, _From}, NotUsed}),
+    erlang:display({delete,{?MODULE, _From}, NotUsed}),
     gl:deleteBuffers(NotUsed).
